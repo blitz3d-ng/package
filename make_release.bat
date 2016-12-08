b4f0fd8644e9ac1b91c7b67578ddf322ac0b7cf0
Kevin Primm <kfprimm@gmail.com>
2016-12-08 15:30:52 -0500

    Added mediaview compilation to make_release.bat. Fixes #2.

--- make_release.bat
+++ make_release.bat
@@ -4,6 +4,10 @@ premake5 vs2015 && devenv build\blitz3d.sln /rebuild "release|win32" || exit /b
 rem echo "Building x64..."
 rem premake5 vs2015 && devenv build\blitz3d.sln /rebuild "release|win64" || exit /b
 
+echo "Compiling mediaview..."
+set blitzpath=_release
+_release\bin\blitzcc.exe -o _release\bin\mediaview.exe _release\mediaview\mediaview.bb
+
 echo "Removing intermediate files..."
 del /F _release\bin\.vs
 del /Q _release\bin\*.ilk _release\bin\*.pcb _release\bin\*.pdb _release\bin\*.lib _release\bin\*.exp
