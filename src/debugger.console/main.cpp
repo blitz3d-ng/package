303d6f89e256f499f1f999771378072f9283009b
Kevin Primm <kfprimm@gmail.com>
2017-04-28 13:48:12 -0400

    Added better output to console debugger.

--- src/debugger.console/main.cpp
+++ src/debugger.console/main.cpp
@@ -1,20 +1,27 @@
 
 #include "../../stdutil/stdutil.h"
-#include "../../debugger/debugger.h"
+#include <bb/blitz/debug.h>
 using namespace std;
 
 class ConsoleDebugger : public Debugger{
+protected:
+	string file;
+	int row,col;
 public:
 	void debugRun(){}
 	void debugStop(){}// bbruntime_panic(0); }
-	void debugStmt( int srcpos,const char *file ){}
+	void debugStmt( int srcpos,const char *f ){
+		file=f;
+		row=(srcpos>>16)&0xffff;
+		col=srcpos&0xffff;
+	}
 	void debugEnter( void *frame,void *env,const char *func ){}
 	void debugLeave(){}
 	void debugLog( const char *msg ){
     cout<<msg<<endl;
   }
 	void debugMsg( const char *e,bool serious ){
-		cerr<<e<<endl;
+		cerr<<file<<"["<<row<<","<<col<<"]: "<<e<<endl;
 	}
 	void debugSys( void *msg ){}
 };
