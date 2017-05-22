5a6375a0cfc43b6010184e902c6810d2e7675524
Kevin Primm <kfprimm@gmail.com>
2017-05-22 14:28:08 -0400

    Incremental Assimp loader progress.

--- src/runtime/bb/blitz3d/loader_assimp.cpp
+++ src/runtime/bb/blitz3d/loader_assimp.cpp
@@ -8,15 +8,21 @@ static bool conv,flip_tris;
 static Transform conv_tform;
 
 MeshModel *Loader_Assimp::parseNode( const struct aiNode* nd ){
-	MeshLoader::beginMesh();
+	MeshModel *node=d_new MeshModel();
 
-	Brush b;
+	aiVector3D pos, scl;
+	aiQuaternion rot;
+	nd->mTransformation.Decompose( scl,rot,pos );
 
-	int tcount=0;
+	node->setLocalPosition( Vector( pos[0],pos[1],pos[2] ) );
+	node->setLocalScale( Vector( scl[0],scl[1],scl[2] )*Vector(1,1,-1) );
+	node->setLocalRotation( Quat( rot.w,Vector( rot.x,rot.y,rot.z ) ) );
 
-	for ( int n=0;n<nd->mNumMeshes;++n ){
+	for( int n=0;n<nd->mNumMeshes;++n ){
 		const struct aiMesh* mesh=scene->mMeshes[nd->mMeshes[n]];
 		const aiMaterial *mtl=scene->mMaterials[mesh->mMaterialIndex];
+
+		Brush b;
 
 		for( int ti=0;ti<mtl->GetTextureCount( aiTextureType_DIFFUSE );ti++ ){
 			aiString path;
@@ -32,57 +38,53 @@ MeshModel *Loader_Assimp::parseNode( const struct aiNode* nd ){
 			if( diffuse.a ) b.setAlpha( diffuse.a );
 		}
 
-		for ( int t=0;t<mesh->mNumFaces;++t ) {
+		MeshLoader::beginMesh();
+
+		for( int i=0;i<mesh->mNumVertices;++i ){
+			Surface::Vertex v;
+
+			v.coords=Vector(&mesh->mVertices[i].x)*Vector(1,1,-1);
+			if( mesh->mColors[0] ) {
+				// v.color=Vector(&mesh->mColors[0][i]);
+			}
+			if( mesh->mNormals ) v.normal=Vector(&mesh->mNormals[i].x);
+			if( mesh->HasTextureCoords(0) ) memcpy( v.tex_coords,&mesh->mTextureCoords[0][i].x,2*sizeof(float) );
+			if( mesh->HasTextureCoords(1) ) memcpy( (&v.tex_coords)+2,&mesh->mTextureCoords[1][i].x,2*sizeof(float) );
+
+			MeshLoader::addVertex( v );
+		}
+
+		for( int t=0;t<mesh->mNumFaces;++t ) {
 
 			const struct aiFace* face=&mesh->mFaces[t];
 
 			// TODO: fix this
-			if( face->mNumIndices<3 ){
+			if( face->mNumIndices!=3 ){
+				// cerr<<"[loader.assimp] encountered face with "<<face->mNumIndices<<" indices"<<endl;
 				continue;
 			}
 
-			for( int i=0;i<face->mNumIndices;i++ ){
-				Surface::Vertex v;
+			int v0=face->mIndices[0],v1=face->mIndices[1],v2=face->mIndices[2];
 
-				int vi=face->mIndices[i];
-
-				v.coords=Vector(&mesh->mVertices[vi].x)*Vector(1,1,-1);
-				if( mesh->mColors[0] ) {
-					// v.color=Vector(&mesh->mColors[0][vi]);
-				}
-				if( mesh->mNormals ) v.normal=Vector(&mesh->mNormals[vi].x);
-				if( mesh->HasTextureCoords(0) ) memcpy( v.tex_coords,&mesh->mTextureCoords[0][vi].x,2*sizeof(float) );
-				if( mesh->HasTextureCoords(1) ) memcpy( (&v.tex_coords)+2,&mesh->mTextureCoords[1][vi].x,2*sizeof(float) );
-
-				MeshLoader::addVertex( v );
-				tcount++;
-			}
 			if( !flip_tris ){
-				MeshLoader::addTriangle( tcount-1,tcount-2,tcount-3,b );
+				MeshLoader::addTriangle( v2,v1,v0,b );
 			}else{
-				MeshLoader::addTriangle( tcount-3,tcount-2,tcount-1,b );
+				MeshLoader::addTriangle( v0,v1,v2,b );
 			}
 		}
+
+		MeshModel *m=d_new MeshModel();
+		m->setParent( node );
+		MeshLoader::endMesh( m );
 	}
 
-	MeshModel *m=d_new MeshModel();
-
-	aiVector3D pos, scl;
-	aiQuaternion rot;
-	nd->mTransformation.Decompose( scl,rot,pos );
-
-	m->setLocalPosition( Vector( pos[0],pos[1],pos[2] ) );
-	m->setLocalScale( Vector( scl[0],scl[1],scl[2] )*Vector(1,1,-1) );
-	m->setLocalRotation( Quat( rot.w,Vector( rot.x,rot.y,rot.z ) ) );
-
-	MeshLoader::endMesh( m );
 
 	for ( int n=0;n<nd->mNumChildren;++n ){
 		MeshModel *child=parseNode( nd->mChildren[n] );
-		child->setParent( m );
+		child->setParent( node );
 	}
 
-	return m;
+	return node;
 }
 
 MeshModel *Loader_Assimp::load( const string &f,const Transform &t,int hint ){
