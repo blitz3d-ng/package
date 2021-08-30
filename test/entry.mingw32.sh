92179ce55a07400fde1d14a9553dc8c9508f2ecc
Kevin Primm <kfprimm@gmail.com>
2021-08-29 22:20:01 -0400

    spec updates

--- test/entry.mingw32.sh
+++ test/entry.mingw32.sh
@@ -7,6 +7,7 @@ cp deps/fmod/bin/fmod.dll _release/toolchains/mingw32/bin
 
 # compile an executable...
 wine _release/toolchains/mingw32/bin/blitzcc.exe -r test -o blank.exe test/blank.bb
+cp deps/fmod/bin/fmod.dll test/
 wine test/blank.exe
 
 # run the specs...
--- test/entry.win32.bat
+++ test/entry.win32.bat
@@ -1,9 +1,9 @@
 REM @echo off
 
 SET blitzpath=_release
-COPY deps\fmod\bin\fmod.dll test\
 
 _release\bin\blitzcc.exe -r test -o blank.exe test\blank.bb
+COPY deps\fmod\bin\fmod.dll test\
 test\blank.exe
 
-_release\bin\blitzcc.exe -r test -o blank.exe test\all.bb
+_release\bin\blitzcc.exe -r test test\all.bb
