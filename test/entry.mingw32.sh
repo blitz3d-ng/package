896a7829920bedc2a35da9f37041331f0a609a87
Kevin Primm <kfprimm@gmail.com>
2021-08-29 20:24:26 -0400

    update

--- test/entry.mingw32.sh
+++ test/entry.mingw32.sh
@@ -3,6 +3,7 @@
 set -e
 
 export blitzpath=_release/toolchains/mingw32
+cp deps/fmod/bin/fmod.dll _release/toolchains/mingw32/bin
 
 # compile an executable...
 wine _release/toolchains/mingw32/bin/blitzcc.exe -r test -o blank.exe test/blank.bb
--- test/entry.win32.bat
+++ test/entry.win32.bat
@@ -1,6 +1,7 @@
 REM @echo off
 
 SET blitzpath=_release
+COPY deps\fmod\bin\fmod.dll test\
 
 _release\bin\blitzcc.exe -r test -o blank.exe test\blank.bb
-blank.exe
+test\blank.exe
