ac4438226ce7804a0ec1169acbec7e4c882bfcc3
Kevin Primm <kfprimm@gmail.com>
2023-03-26 15:07:39 -0400

    update

--- test/suite.bat
+++ test/suite.bat
@@ -11,4 +11,8 @@ echo Running suite for %Platform%...
 
 _release\bin\%BLITZCC% -r test test\all.bb || exit /b 1
 
+IF "%Platform%" == "x86" (
+  _release\bin\%BLITZCC% -c -r test -o suite.exe test\all.bb || exit /b 1
+)
+
 echo Success
